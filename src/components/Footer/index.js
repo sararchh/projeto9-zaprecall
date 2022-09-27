89b1dde517af3c8156fbf5fa4802ce9d2418de2f
Sara Rocha de Oliveira <sararocha878@gmail.com>
2022-09-27 18:10:00 -0300

    feature: fix it footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,35 +3,21 @@ import React from 'react';
 import { Footer, Icon } from './styles';
 
 function FooterScreen({ qtdQuestion, counter, rowAnswer }) {
-  const [finished, setFinished] = React.useState(false);
-
-  React.useEffect(() => {
-    verifyCounter();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [counter]);
-
-  const verifyCounter = () => {
-    if (counter === qtdQuestion) {
-      setFinished(true);
-    }
-  }
 
   return (
     <Footer>
       <p>{counter}/{qtdQuestion} CONCLUÍDOS</p>
-      {finished === true &&
-        (<div>
-          {rowAnswer.sort((a, b) => a.index - b.index).map((i, index) => {
-            if (i.value === 'error') {
-              return <Icon key={index} src='./assets/img/close-circle-icon.svg' alt='player' />
-            } else if (i.value === 'help') {
-              return <Icon src='./assets/img/help-circle-icon.svg' alt='player' />
-            } else {
-              return <Icon src='./assets/img/checkmark-circle-icon.svg' alt='player' />
-            }
-          })}
-        </div>)
-      }
+      <div>
+        {rowAnswer.sort((a, b) => a.index - b.index).map((i, index) => {
+          if (i.value === 'error') {
+            return <Icon key={index} src='./assets/img/close-circle-icon.svg' alt='player' />
+          } else if (i.value === 'help') {
+            return <Icon src='./assets/img/help-circle-icon.svg' alt='player' />
+          } else {
+            return <Icon src='./assets/img/checkmark-circle-icon.svg' alt='player' />
+          }
+        })}
+      </div>
 
     </Footer>
   );
